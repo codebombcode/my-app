1d06e5b3bf4ae91917df8e48e38408a2082e8d69
danfeng <danfeng.zh@dtyunxi.com>
2021-06-24 18:45:48 +0800

    feat：基础

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import ReactDOM from "react-dom";
+import React from "react";
+
+
+const jsx = (
+  <div>
+    <h1>mini-react-17</h1>
+    <a href="https://github.com/codebombcode/mini-react-17">mini-react-17</a>
+  </div>
+)
+
+
+ReactDOM.render(jsx, document.getElementById('app'))
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+    clean: true,
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env','@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  plugins: [new HtmlWebpackPlugin({
+    template: './index.html'
+  })],
+}
